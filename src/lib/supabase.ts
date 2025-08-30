db84b6922a43dfe61ef91dfdb4dd4f183d2ca17b
Joaquim Alves <jafn92@gmail.com>
2025-08-30 13:13:14 -0300

    ...

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Verificar se as variáveis de ambiente estão definidas
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://scykmpulfrffkyofvpny.supabase.co'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON || ''
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error(
