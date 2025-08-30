361cafd080bf5b9bdc0661f735581371518b705d
Joaquim Alves <jafn92@gmail.com>
2025-08-30 11:54:03 -0300

    123...

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 // Verificar se as variáveis de ambiente estão definidas
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error(
